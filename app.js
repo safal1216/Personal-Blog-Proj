14f42b4fccce8ba55c1ed66c5dcb1bb8525b87ca
Safal Mehrotra <safalmehrotra1216@gmail.com>
2024-02-04 00:26:55 +0530

    bassic done

--- app.js
+++ app.js
@@ -21,6 +21,20 @@ app.get("/", (req, res)=>{
 })
 })
 
+app.get("/about", (req, res)=>{
+  res.render("about", {
+    aboutContent: aboutContent
+})
+})
+
+app.get("/contact", (req, res)=>{
+  res.render("contact", {
+    contactContent : contactContent
+})
+})
+
+
+
 
 
 
